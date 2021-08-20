ad6b6a3e6651bd683827e2a83bfd994bc4372e36
Sanam <64443718+sanamcha@users.noreply.github.com>
2021-08-20 16:29:59 -0700

    third commit

--- connect4.js
+++ connect4.js
@@ -1,19 +1,9 @@
-/** Connect Four
- *
- * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
- * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
- */
+
 
 const WIDTH = 8;
 const HEIGHT = 6;
-
-let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *   board = array of rows, each row is array of cells  (board[y][x])
- */
+let currPlayer = 1; 
+let board = []; 
 
 function makeBoard() {
   for (let y = 0; y < HEIGHT; y++) {
@@ -21,12 +11,8 @@ function makeBoard() {
   }
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
   const board = document.getElementById('board');
-
-  // make column tops (clickable area for adding a piece to that column)
   const top = document.createElement('tr');
   top.setAttribute('id', 'column-top');
   top.addEventListener('click', handleClick);
@@ -39,7 +25,6 @@ function makeHtmlBoard() {
 
   board.append(top);
 
-  // make main part of board
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement('tr');
 
@@ -53,8 +38,6 @@ function makeHtmlBoard() {
   }
 }
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(x) {
   for (let y = HEIGHT - 1; y >= 0; y--) {
     if (!board[y][x]) {
@@ -64,72 +47,51 @@ function findSpotForCol(x) {
   return null;
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
   const piece = document.createElement('div');
   piece.classList.add('piece');
   piece.classList.add(`p${currPlayer}`);
  
-
   const spot = document.getElementById(`${y}-${x}`);
   spot.append(piece);
 }
-
-/** endGame: announce game end */
 
 function endGame(msg) {
   
   const endMessage = document.getElementById('end-message');
   endMessage.innerText = 'GAME OVER!!'; 
   
-  
 }
 
-/** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
-  // get x from ID of clicked cell
   const x = +evt.target.id;
-
-  // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
 
-  // place piece in board and add to HTML table
   board[y][x] = currPlayer;
   placeInTable(y, x);
-  
-  // check for win
+
   if (checkForWin()) {
     let p =document.querySelector('p');
     p.innerHTML = `Player ${currPlayer} won!`;
     return endGame(`Player ${currPlayer} won!`);
-    
-    
+       
   }
-  
-  // check for tie
+
   if (board.every(row => row.every(cell => cell))) {
     
     let p =document.querySelector('p');
     p.innerHTML = "The Game is Tie";
     return endGame('Tie!');
   }
-    
-  // switch players
+
   currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
 
     return cells.every(([y, x]) =>
         y >= 0 &&
@@ -142,14 +104,12 @@ function checkForWin() {
 
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
-      // get "check list" of 4 cells (starting here) for each of the different
-      // ways to win
+   
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
-      // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
